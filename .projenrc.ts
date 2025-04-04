f3f55347e47481b630e0c4f58c67a1fc500aa896
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-04-05 05:20:50 +0900

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Yutaro Sakamoto',
+  authorAddress: 'mail@yutaro-sakamoto.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.8.0',
+  name: 'proxy-nat-vpn',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/mail/proxy-nat-vpn.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
