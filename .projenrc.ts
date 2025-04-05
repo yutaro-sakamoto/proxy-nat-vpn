f8c7808f056d4ddba17425e55bc9cff2c19ed577
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-04-05 12:22:16 +0900

    ci: run tests on push

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,13 @@
 import { awscdk } from 'projen';
+import { YamlFile } from 'projen';
+
+const releaseBranch = 'release';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Yutaro Sakamoto',
   authorAddress: 'mail@yutaro-sakamoto.com',
   cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'release',
+  defaultReleaseBranch: releaseBranch,
   jsiiVersion: '~5.8.0',
   name: 'proxy-nat-vpn',
   projenrcTs: true,
@@ -14,4 +18,87 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+new YamlFile(project, '.github/workflows/push.yml', {
+  obj: {
+    name: 'push',
+    on: {
+      push: {
+        'branches-ignore': [releaseBranch],
+      },
+    },
+    concurrency: {
+      'group': '${{ github.workflow }}-${{ github.ref }}',
+      'cancel-in-progress': true,
+    },
+    permissions: {
+      'contents': 'read',
+    },
+    jobs: {
+      'check-workflows': {
+        permissions: {
+          contents: 'read',
+        },
+        uses: './.github/workflows/check-workflows.yml',
+      },
+      'test': {
+        needs: 'check-workflows',
+        secrets: 'inherit',
+        uses: './.github/workflows/test.yml',
+        with: {
+          environment: 'dev',
+        },
+      },
+    },
+  },
+});
+
+new YamlFile(project, '.github/workflows/test.yml', {
+  obj: {
+    name: 'test',
+    on: {
+      workflow_call: {},
+    },
+    jobs: {
+      test: {
+        'runs-on': 'ubuntu-latest',
+        'steps': [
+          {
+            name: 'Checkout',
+            uses: 'actions/checkout@v4',
+          },
+          {
+            uses: 'actions/setup-node@v4',
+            with: {
+              'node-version': '22',
+              'cache': 'yarn',
+              'cache-dependency-path': 'yarn.lock',
+            },
+          },
+          {
+            run: 'yarn install',
+          },
+          {
+            name: 'Check format by Prettier',
+            run: 'npx prettier src test example --check',
+          },
+          {
+            name: 'Check by ESLint',
+            run: 'yarn eslint',
+          },
+          {
+            name: 'Tests',
+            run: 'yarn test',
+          },
+          {
+            name: 'Check docs',
+            run: 'npx typedoc --validation src/*.ts',
+          },
+        ],
+      },
+    },
+  },
+});
+
+
 project.synth();
